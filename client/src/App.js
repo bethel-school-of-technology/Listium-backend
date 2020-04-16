9462a3e6fb60f3982323485abd89c687b3888ef1
Michael R <michael.d.riddering@gmail.com>
2020-04-15 18:07:52 -0600

    It's working in one code base!

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Customers from './components/customers';
+import Events from './components/events';
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Express Starter</h1>
         </header>
-        <Customers />
+        <Events />
       </div>
     );
   }
--- server.js
+++ server.js
@@ -2,14 +2,52 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
-
-  res.json(customers);
+app.get('/api/events', (req, res) => {
+  let events = [
+    {
+        id: 1,
+        eventName: "The day we got our first dog!",
+        eventCategory: "Special Day",
+        eventDate: "April 13"
+    },
+    {
+        id: 2,
+        eventName: "Michael's Birthday",
+        eventCategory: "Birthday",
+        eventDate: "February 23"
+    },
+    {
+        id: 3,
+        eventName: "Parent's Anniversary",
+        eventCategory: "Anniversary",
+        eventDate: "March 11"
+    },
+    {
+        id: 4,
+        eventName: "Jim's Birthday",
+        eventCategory: "Birthday",
+        eventDate: "December 25"
+    },
+    {
+        id: 5,
+        eventName: "The day I first met my wife",
+        eventCategory: "Special Day",
+        eventDate: "January 12"
+    },
+    {
+        id: 6,
+        eventName: "Michael Scott's Birthday",
+        eventCategory: "Birthday",
+        eventDate: "November 25"
+    },
+    {
+        id: 7,
+        eventName: "Richa's Birthday",
+        eventCategory: "Birthday",
+        eventDate: "May 7"
+    }
+]
+  res.json(events);
 });
 
 const port = 5000;
