a6538de140eac79f3e4a3290e00e3d13af77d582
Michael <brewtic@gmail.com>
2020-04-20 15:31:10 -0700

    Sequelize/Users Migration

---
+++ migrations/1-users.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * createTable "users", deps: []
+ *
+ **/
+
+var info = {
+    "revision": 1,
+    "name": "users",
+    "created": "2020-04-20T22:22:01.982Z",
+    "comment": ""
+};
+
+var migrationCommands = [{
+    fn: "createTable",
+    params: [
+        "users",
+        {
+            "UserId": {
+                "type": Sequelize.INTEGER,
+                "field": "UserId",
+                "primaryKey": true,
+                "autoIncrement": true,
+                "allowNull": false
+            },
+            "FirstName": {
+                "type": Sequelize.STRING,
+                "field": "FirstName"
+            },
+            "LastName": {
+                "type": Sequelize.STRING,
+                "field": "LastName"
+            },
+            "Email": {
+                "type": Sequelize.STRING,
+                "field": "Email",
+                "unique": true
+            },
+            "Username": {
+                "type": Sequelize.STRING,
+                "field": "Username",
+                "unique": true
+            },
+            "Password": {
+                "type": Sequelize.STRING,
+                "field": "Password"
+            },
+            "Admin": {
+                "type": Sequelize.BOOLEAN,
+                "field": "Admin",
+                "allowNull": false,
+                "defaultValue": false
+            },
+            "createdAt": {
+                "type": Sequelize.DATE,
+                "field": "createdAt",
+                "allowNull": false
+            },
+            "updatedAt": {
+                "type": Sequelize.DATE,
+                "field": "updatedAt",
+                "allowNull": false
+            }
+        },
+        {}
+    ]
+}];
+
+module.exports = {
+    pos: 0,
+    up: function(queryInterface, Sequelize)
+    {
+        var index = this.pos;
+        return new Promise(function(resolve, reject) {
+            function next() {
+                if (index < migrationCommands.length)
+                {
+                    let command = migrationCommands[index];
+                    console.log("[#"+index+"] execute: " + command.fn);
+                    index++;
+                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                }
+                else
+                    resolve();
+            }
+            next();
+        });
+    },
+    info: info
+};
---
+++ migrations/20200420222055-create-users.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      FirstName: {
+        type: Sequelize.STRING
+      },
+      LastName: {
+        type: Sequelize.STRING
+      },
+      Email: {
+        type: Sequelize.STRING
+      },
+      Username: {
+        type: Sequelize.STRING
+      },
+      Password: {
+        type: Sequelize.STRING
+      },
+      Admin: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
