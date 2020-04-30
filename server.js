519cd6d867580ff374f2d7258eb2dc5637399ec2
Michael <brewtic@gmail.com>
2020-04-30 15:17:57 -0700

    crud....

--- server.js
+++ server.js
@@ -2,79 +2,118 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const models = require("./models");
+const mysql = require("mysql2");
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", function (req, res, next) {
-  res.render("index", { title: "Listium" });
+app.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
+//EVENTS//////////////////////////////////////////////////////////////////
 app.get("/events", (req, res) => {
-  models.users.findall({
-        include: [models.events],
-      })
-      .then((eventsFound) => {
-        res.render("events", {
-          events: { eventsFound },
-        });
+  models.users
+    .findall({
+      include: [models.events],
+    })
+    .then((eventsFound) => {
+      res.render("events", {
+        events: { eventsFound },
       });
+    });
   res.json({ events });
 });
 
-app.get("/api/events", (req, res) => {
-  let events = [
-    {
-      id: 1,
-      eventName: "The day we got our first dog!",
-      eventCategory: "Special Day",
-      eventDate: "April 13",
-    },
-    {
-      id: 2,
-      eventName: "Michael's Birthday",
-      eventCategory: "Birthday",
-      eventDate: "February 23",
-    },
-    {
-      id: 3,
-      eventName: "Parent's Anniversary",
-      eventCategory: "Anniversary",
-      eventDate: "March 11",
-    },
-    {
-      id: 4,
-      eventName: "Jim's Birthday",
-      eventCategory: "Birthday",
-      eventDate: "December 25",
-    },
-    {
-      id: 5,
-      eventName: "The day I first met my wife",
-      eventCategory: "Special Day",
-      eventDate: "January 12",
-    },
-    {
-      id: 6,
-      eventName: "Michael Scott's Birthday",
-      eventCategory: "Birthday",
-      eventDate: "November 25",
-    },
-    {
-      id: 7,
-      eventName: "Richa's Birthday",
-      eventCategory: "Birthday",
-      eventDate: "May 7",
-    },
-  ];
-  res.json({ events });
+app.get("/event/:id", (req, res) => {
+  models.events.findByPk(parseInt(req.params.id)).then((event) => {
+    if (user) {
+      res.json("events", {
+        EventName: event.eventName,
+      });
+    } else {
+      res.send("Event not found");
+    }
+  });
+});
+
+//SIGNUP//////////////////////////////////////////////////////////////////
+app.get("/signup", function (req, res, next) {
+  res.render("signup");
+});
+
+app.post("/signup", function (req, res, next) {
+  models.users
+    .findOrCreate({
+      where: {
+        Username: req.body.username,
+      },
+      defaults: {
+        FirstName: req.body.firstName,
+        LastName: req.body.lastName,
+        Email: req.body.email,
+        Password: req.body.password,
+      },
+    })
+    .spread(function (result, created) {
+      if (created) {
+        res.redirect("login"); //<---Change this line to redirect to the login screen
+      } else {
+        res.send("This username is taken!");
+      }
+    });
+});
+//LOGIN////////////////////////////////////////////////////////////////
+
+app.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+app.post("/login", function (req, res, next) {
+  models.users
+    .findOne({
+      where: {
+        Username: req.body.username,
+        Password: req.body.password,
+      },
+    })
+    .then((user) => {
+      if (user) {
+        res.redirect("profile/" + user.UserId);
+      } else {
+        res.send("Invalid login!");
+      }
+    });
+});
+
+//PROFILE//////////////////////////////////////////////////////////////////
+
+app.get('/profile', function (req, res, next) {
+  if (req.user) {
+    models.users
+      .findByPk(parseInt(req.user.UserId))
+      .then(user => {
+        if (user) {
+          res.render('profile', {
+            FirstName: user.FirstName,
+            LastName: user.LastName,
+            Email: user.Email,
+            Username: user.Username
+          });
+        } else {
+          res.send('User not found');
+        }
+      });
+  } else {
+    res.redirect('/users/login');
+  }
 });
 
 
 models.sequelize.sync().then(function () {
-  console.log("DB is running!")
+  console.log("DB is running!");
 });
 
 app.listen(port, () => `Server running on port ${port}`);
