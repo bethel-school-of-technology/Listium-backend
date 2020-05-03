d3cf40a3be914c1b3ad6b49b7696b468496b97d2
Michael <brewtic@gmail.com>
2020-05-03 13:53:23 -0700

    Updated routes

--- server.js
+++ server.js
@@ -9,17 +9,16 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+app.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 //EVENTS////z//////////////////////////////////////////////////////////////
 app.get("/events", (req, res) => {
-  models.events.findAll().then(events => {
-    console.log(events)
-    res.json(events)
-  })
-
+  models.events.findAll().then((events) => {
+    console.log(events);
+    res.json(events);
+  });
 });
 
 // app.post("/events", events.create);
@@ -31,10 +30,48 @@ app.get("/event/:id", (req, res) => {
       res.json({
         EventName: event.eventName,
         EventCategory: event.eventCategory,
-        EventDate: event.eventDate
+        EventDate: event.eventDate,
       });
     } else {
       res.send("Event not found");
+    }
+  });
+});
+
+app.post("/event", function (req, res, next) {
+  models.events
+    .findOrCreate({
+      where: {
+        eventName: req.body.eventName,
+      },
+      defaults: {
+        eventCategory: req.body.eventCategory,
+        eventDate: req.body.eventDate,
+      },
+    })
+    .spread(function (result, created) {
+      if (created) {
+        res.send("EventCreated");
+      } else {
+        res.send("This event already exists!");
+      }
+    });
+});
+
+
+//PROFILE//////////////////////////////////////////////////////////////////
+
+app.get("/profile/:id", function (req, res, next) {
+  models.users.findByPk(parseInt(req.params.id)).then((user) => {
+    if (user) {
+      res.json({
+        FirstName: user.FirstName,
+        LastName: user.LastName,
+        Email: user.Email,
+        Username: user.Username,
+      });
+    } else {
+      res.send("User not found");
     }
   });
 });
@@ -45,18 +82,18 @@ app.post("/signup", function (req, res, next) {
   models.users
     .findOrCreate({
       where: {
-        Username: req.body.username,
+        Username: req.body.Username,
       },
       defaults: {
-        FirstName: req.body.firstName,
-        LastName: req.body.lastName,
-        Email: req.body.email,
-        Password: req.body.password,
+        FirstName: req.body.FirstName,
+        LastName: req.body.LastName,
+        Email: req.body.Email,
+        Password: req.body.Password,
       },
     })
     .spread(function (result, created) {
       if (created) {
-        res.send("success"); //<---Change this line to redirect to the login screen
+        res.send("success");
       } else {
         res.send("This username is taken!");
       }
@@ -68,8 +105,8 @@ app.post("/login", function (req, res, next) {
   models.users
     .findOne({
       where: {
-        Username: req.body.username,
-        Password: req.body.password,
+        Username: req.body.Username,
+        Password: req.body.Password,
       },
     })
     .then((user) => {
@@ -81,28 +118,7 @@ app.post("/login", function (req, res, next) {
     });
 });
 
-//PROFILE//////////////////////////////////////////////////////////////////
 
-app.get('/profile', function (req, res, next) {
-  if (req.user) {
-    models.users
-      .findByPk(parseInt(req.user.UserId))
-      .then(user => {
-        if (user) {
-          res.render('profile', {
-            FirstName: user.FirstName,
-            LastName: user.LastName,
-            Email: user.Email,
-            Username: user.Username
-          });
-        } else {
-          res.send('User not found');
-        }
-      });
-  } else {
-    res.redirect('/users/login');
-  }
-});
 
 
 models.sequelize.sync().then(function () {
